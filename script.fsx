96ede66e65b75d6b1f8317eedcd712020d4688ed
Tamizhvendan S <tamizh88@gmail.com>
2017-12-09 10:27:02 +0530

    adds record parsing

--- script.fsx
+++ script.fsx
@@ -29,8 +29,7 @@ let parseBool = tryParseWith Boolean.TryParse
 let parseString = tryParseWith (fun s -> (true,s))
 
 
-// string -> EnvVarParseResult<'T>
-let parsePrimitive<'T> (envVarName : string) : EnvVarParseResult<'T> =
+let parsePrimitive<'T> (envVarName : string) =
   let wrap(p : string -> 'a) = 
     envVarName
     |> unbox<string -> EnvVarParseResult<'T>> p 
@@ -39,12 +38,9 @@ let parsePrimitive<'T> (envVarName : string) : EnvVarParseResult<'T> =
   | Shape.Int32 -> wrap parseInt
   | Shape.String -> wrap parseString
   | Shape.Bool -> wrap parseBool
-  | _ -> NotSupported "unknown target type"
-
-
+  | _ -> sprintf "unknown target type - %A" typedefof<'T> |> NotSupported
 let envVarNameRegEx = 
   Regex("([^A-Z]+|[A-Z][^A-Z]+|[A-Z]+)", RegexOptions.Compiled)
-
 let canonicalizeEnvVarName name =
   let subStrings =
     envVarNameRegEx.Matches name
@@ -52,14 +48,22 @@ let canonicalizeEnvVarName name =
     |> Seq.map (fun (m : Match) -> m.Value.ToUpperInvariant())
     |> Seq.toArray
   String.Join("_", subStrings)
-  
+let private parseRecordField (field : IShapeWriteMember<'RecordType>) = 
+  let envVarName = canonicalizeEnvVarName field.Label
+  field.Accept { 
+    new IWriteMemberVisitor<'RecordType, EnvVarParseResult<'T>> with
+      member __.Visit (shape : ShapeWriteMember<'RecordType, 'FieldType>) =
+        parsePrimitive<'FieldType> envVarName |> printfn "%A"
+        parsePrimitive<'T> envVarName
+    }
 let parseRecord<'T> () =
   match shapeof<'T> with
-  | Shape.FSharpRecord (:? ShapeFSharpRecord<'T> as shape) -> 
+  | Shape.FSharpRecord (:? ShapeFSharpRecord<'T> as shape) ->
     shape.Fields
-    |> Seq.iter (fun f -> 
-      let envVarName = canonicalizeEnvVarName f.Label
-      printfn "%s, %s" envVarName f.Member.Type.Name)
+    |> Seq.iter (fun field -> 
+      parseRecordField field 
+      |> ignore
+    )
   | _ -> failwith "not supported"
 
 type Config = {
